d41715b1ffd039c39f210cbe0ee9880ed1ac06e0
Tanmay Kulkarni <tanmaykulkarni1413@gmail.com>
2025-12-08 18:44:38 +0530

    customer model crud and filters

--- lead_management/views.py
+++ lead_management/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication 
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status , filters
 from rest_framework import viewsets
 from .models import Customer
 from .serializers import CustomerSerializer
@@ -13,5 +13,7 @@ class CustomerViewsets(viewsets.ModelViewSet):
   serializer_class = CustomerSerializer
   authentication_classes = [JWTAuthentication]   
   permission_classes = [IsAuthenticated]
+  filter_backends = [filters.SearchFilter]
+  search_fields = ['^name', '=email', 'contact_number', 'city','state' ,'site_city','site_state']
 
   
