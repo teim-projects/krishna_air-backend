6644d187db17a43d333ad320d81bc0fe15c5fb86
Tanmay KLulkarni <tanmaykulkarni1413.com>
2026-01-14 13:01:02 +0530

    Update BASE_URL in .env and add new routes for acSubTypes and brand in product_management URLs

--- product_management/urls.py
+++ product_management/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import acTypeViewSet
+from .views import acTypeViewSet , acSubTypesViewSet, brandViewSet
 
 router = DefaultRouter()
 router.register(r'actype', acTypeViewSet, basename='actype')
+router.register(r'ac-subtypes', acTypeViewSet, basename='ac-subtypes')
+router.register(r'ac-brand', acTypeViewSet, basename='brand')
 
 urlpatterns = [
     
