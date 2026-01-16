8130cdf6bd7a5fdbcf974362e780c506a4801e62
Tanmay KLulkarni <tanmaykulkarni1413.com>
2026-01-16 17:07:10 +0530

    Add product inventory viewset to URL routing

--- product_management/urls.py
+++ product_management/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import acTypeViewSet , acSubTypesViewSet, brandViewSet , productModelViewSet , productVariabtViewSet
+from .views import acTypeViewSet , acSubTypesViewSet, brandViewSet , productModelViewSet , productVariabtViewSet, productInventoryViewSet
 
 router = DefaultRouter()
 router.register(r'actype', acTypeViewSet, basename='actype')
@@ -9,7 +9,7 @@ router.register(r'ac-subtypes', acSubTypesViewSet, basename='ac-subtypes')
 router.register(r'ac-brand', brandViewSet, basename='brand')
 router.register(r'product-model', productModelViewSet, basename='product-model')
 router.register(r'product-variant', productVariabtViewSet, basename='product-variant')
-# router.register(r'product-inventory',)
+router.register(r'product-inventory',productInventoryViewSet , basename='product-inventory')
 
 urlpatterns = [
     
