253aa637f9e95a25ae676a7c5f83d0c40f9ec4f2
Pooja Diwate <diwatepooja0110@gmail.com>
2026-04-09 10:58:02 +0530

    added quto suggestion thankyou note

--- quotation/urls.py
+++ quotation/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuotationViewSet
+from .views import QuotationViewSet, thank_you_suggestions
 
 router = DefaultRouter()
 
 router.register(r'quotation',QuotationViewSet,basename='quotation')
 
-urlpatterns = []
+urlpatterns = [
+    path('thank-you-suggestions/', thank_you_suggestions, name='thank_you_suggestions'),
+]
 
 urlpatterns += router.urls
