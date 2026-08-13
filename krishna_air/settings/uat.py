2b07e8233fee4f5fecabab512a35b4ebbae96686
Pooja Diwate <diwatepooja0110@gmail.com>
2026-08-13 11:53:51 +0530

    domain shifting

--- krishna_air/settings/uat.py
+++ krishna_air/settings/uat.py
@@ -2,22 +2,22 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = [
-    "dsaqua.online",
-    "www.dsaqua.online",
+    "krisnahvac.in",
+    "www.krisnahvac.in",
     "backend-uat",
     "localhost",
     "127.0.0.1",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://dsaqua.online",
-    "https://www.dsaqua.online",
-    "http://dsaqua.online",
+    "https://krisnahvac.in",
+    "https://www.krisnahvac.in",
+    "http://krisnahvac.in",
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://dsaqua.online",
-    "https://www.dsaqua.online",
+    "https://krisnahvac.in",
+    "https://www.krisnahvac.in",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
